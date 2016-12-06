27617271d7783c8b8057f57ebfa7bd454a3e5fff
Timo Furrer <tuxtimo@gmail.com>
2016-12-06 22:56:55 +0100

    Implement CPU utilization calculation

--- src/devheart.c
+++ src/devheart.c
@@ -20,43 +20,110 @@
 #include <linux/sched.h>
 #include <linux/cpumask.h>  // for_each_possible_cpu
 #include <linux/kernel_stat.h>  // kcpustat_cpu
+#include <linux/delay.h>  // msleep_interruptible
+#include <linux/tick.h> // get_cpu_idle_time_us
 
 // module header information
-MODULE_LICENSE("MIT");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Timo Furrer");
 MODULE_DESCRIPTION("Kernel Module which illustrates a Tuxs heart.");
 
 // device name to use
 #define DEVICE_NAME "heart"
 
+// interval in milliseconds in which to measure CPU utilization
+#define CPU_MEASURE_INTERVAL 1000
+
 // kernel thread instance
 struct task_struct *task;
 
-u64 cpu_stat(void) {
+static u64 get_idle_time(int cpu)
+{
+    u64 idle, idle_time = -1ULL;
+
+    if (cpu_online(cpu)) {
+        idle_time = get_cpu_idle_time_us(cpu, NULL);
+    }
+
+    if (idle_time == -1ULL) {
+        /* !NO_HZ or cpu offline so we can rely on cpustat.idle */
+        idle = kcpustat_cpu(cpu).cpustat[CPUTIME_IDLE];
+    }
+    else {
+        idle = usecs_to_cputime64(idle_time);
+    }
+
+    return idle;
+}
+
+static u64 get_iowait_time(int cpu)
+{
+    u64 iowait, iowait_time = -1ULL;
+
+    if (cpu_online(cpu)) {
+        iowait_time = get_cpu_iowait_time_us(cpu, NULL);
+    }
+
+    if (iowait_time == -1ULL) {
+        /* !NO_HZ or cpu offline so we can rely on cpustat.iowait */
+        iowait = kcpustat_cpu(cpu).cpustat[CPUTIME_IOWAIT];
+    }
+    else {
+        iowait = usecs_to_cputime64(iowait_time);
+    }
+
+    return iowait;
+}
+
+void cpu_stat(u64 *idle_time, u64 *total_time) {
     int i;
-    // TODO: merge those two variables
-    u64 user, system;
+    u64 user, nice, system, idle, iowait, irq, softirq, steal = 0;
 
     for_each_possible_cpu(i) {
         user += kcpustat_cpu(i).cpustat[CPUTIME_USER];
+        nice += kcpustat_cpu(i).cpustat[CPUTIME_NICE];
         system += kcpustat_cpu(i).cpustat[CPUTIME_SYSTEM];
+        idle += get_idle_time(i);
+        iowait += get_iowait_time(i);
+        irq += kcpustat_cpu(i).cpustat[CPUTIME_IRQ];
+        softirq += kcpustat_cpu(i).cpustat[CPUTIME_SOFTIRQ];
+        steal += kcpustat_cpu(i).cpustat[CPUTIME_STEAL];
     }
 
-    return user + system;
+    *idle_time = idle + iowait;
+    *total_time = *idle_time + user + nice + system + irq + softirq + steal;
 }
 
 int measure_cpu_utilization(void *data) {
-    u64 foo;
-    pr_info("in thread function");
+    static u64 previous_cpu_idle_time = 0;
+    static u64 previous_cpu_total_time = 0;
 
-    foo = cpu_stat();
-    pr_info("got cpu stat: %llu", cputime64_to_clock_t(foo));
-    foo = cpu_stat();
-    pr_info("got cpu stat: %llu", cputime64_to_clock_t(foo));
-    /*while(!kthread_should_stop()) {*/
-        /*pr_info("in kthread function loop");*/
-        /*schedule();*/
-    /*}*/
+    u64 current_cpu_idle_time, current_cpu_total_time = 0;
+    u64 delta_idle_time, delta_total_time = 0;
+
+    // current CPU utilization in percentage
+    int cpu_utilization = 0;
+
+    // initial fetch of cpu times
+    cpu_stat(&previous_cpu_idle_time, &previous_cpu_total_time);
+
+    while(!kthread_should_stop()) {
+        msleep_interruptible(CPU_MEASURE_INTERVAL);
+
+        // get current CPU stats
+        cpu_stat(&current_cpu_idle_time, &current_cpu_total_time);
+
+        // calculate CPU stat difference since last measurement
+        delta_idle_time = current_cpu_idle_time - previous_cpu_idle_time;
+        delta_total_time = current_cpu_total_time - previous_cpu_total_time;
+
+        // calculate CPU usage in percentage
+        cpu_utilization = (1000 * (delta_total_time - delta_idle_time) / delta_total_time + 5) / 10;
+        pr_info("Current CPU Utilization is %d%%", cpu_utilization);
+
+        previous_cpu_idle_time = current_cpu_idle_time;
+        previous_cpu_total_time = current_cpu_total_time;
+    }
 
     return 0;
 }
@@ -143,10 +210,10 @@ static int __init heart_init(void)
 
 static void __exit heart_exit(void)
 {
+    kthread_stop(task);
+
     misc_deregister(&heart_dev);
     pr_info("deregistered heart device!\n");
-
-    kthread_stop(task);
 }
 
 module_init(heart_init);
